26ee763380772be7299fc4f51aebf1bda6579b8b
edanurcoskun <eda.cskn.01@gmail.com>
2020-01-15 19:07:18 +0300

    Create giris.aspx.cs

---
+++ giris.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["kullaniciid"] != null)
+            {
+                Response.Redirect("todo.aspx");
+            }
+
+        }
+        protected void Girisbutonu_Click(object sender, EventArgs e)
+        {
+
+            bool isAdmin = false;
+            SqlConnection con = new SqlConnection("Data Source = PC; Initial Catalog = PROJECT; Integrated Security = True");
+            SqlCommand cmd = new SqlCommand("Select * from register where username=@UNAME and password=@UPASS", con);
+            cmd.Parameters.AddWithValue("UNAME", Kullaniciadi.Text);
+            cmd.Parameters.AddWithValue("UPASS", Sifre.Text);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (!dr.Read())
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Hatalı giriş yaptınız lütfen tekrar deneyin...');</script>");
+            }
+            else
+            {
+
+                Session["kullaniciid"] = dr["id"];
+                Session["kullaniciadi"] = dr["name"] + " " + dr["surname"];
+                Session["username"] = dr["username"];
+                isAdmin = Convert.ToBoolean(dr["isadmin"]);
+                con.Close();
+
+                if (isAdmin)
+                {
+                    Response.Redirect("yoneticitodo.aspx");
+                }
+                else
+                {
+                    Response.Redirect("todo.aspx");
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
